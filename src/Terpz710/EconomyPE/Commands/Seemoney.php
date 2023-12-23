c702991a302f370baed8f39ae3a5a657dec8f9cf
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-12-23 13:04:19 -0800

    SeeMoney - PluginOwned and permission

--- src/Terpz710/EconomyPE/Commands/Seemoney.php
+++ src/Terpz710/EconomyPE/Commands/Seemoney.php
@@ -6,20 +6,30 @@ namespace Terpz710\EconomyPE\Commands;
 
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
+use pocketmine\plugin\PluginOwned;
+use pocketmine\plugin\Plugin;
 use pocketmine\player\Player;
 use pocketmine\Server;
 
 use Terpz710\EconomyPE\Money;
 
-class Seemoney extends Command
+class Seemoney extends Command implements PluginOwned
 {
-    public function __construct()
+
+    private $plugin;
+    
+    public function __construct(Money $plugin)
     {
+        $this->plugin = $plugin;
         $command = explode(":", Money::getConfigReplace("seemoney"));
         parent::__construct($command[0]);
         if (isset($command[1])) $this->setDescription($command[1]);
         $this->setAliases(Money::getConfigValue("seemoney_aliases"));
         $this->setPermission("economype.seemoney");
+    }
+
+    public function getOwningPlugin(): Plugin {
+        return $this->plugin;
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args)
