396ed1af083f21fa5c39f4c3ff26a6351b5d1427
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-12-23 13:06:13 -0800

    SetMoney - PluginOwned and permission

--- src/Terpz710/EconomyPE/Commands/SetMoney.php
+++ src/Terpz710/EconomyPE/Commands/SetMoney.php
@@ -6,20 +6,27 @@ namespace Terpz710\EconomyPE\Commands;
 
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
+use pocketmine\plugin\PluginOwned;
+use pocketmine\plugin\Plugin;
 use pocketmine\player\Player;
 use pocketmine\Server;
 
 use Terpz710\EconomyPE\Money;
 
-class SetMoney extends Command
+class SetMoney extends Command implements PluginOwned
 {
-    public function __construct()
+    public function __construct(Money $plugin)
     {
+        $this->plugin = $plugin;
         $command = explode(":", Money::getConfigReplace("setmoney"));
         parent::__construct($command[0]);
         if (isset($command[1])) $this->setDescription($command[1]);
         $this->setAliases(Money::getConfigValue("setmoney_aliases"));
         $this->setPermission("economype.setmoney");
+    }
+
+    public function getOwningPlugin(): Plugin {
+        return $this->plugin;
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args)
