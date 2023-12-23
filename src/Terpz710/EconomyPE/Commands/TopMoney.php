52c6dc0b329caa5f3782f3efa4cdd04ae8438f05
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-12-23 13:10:02 -0800

    TopMoney - PluginOwned and permission

--- src/Terpz710/EconomyPE/Commands/TopMoney.php
+++ src/Terpz710/EconomyPE/Commands/TopMoney.php
@@ -6,19 +6,29 @@ namespace Terpz710\EconomyPE\Commands;
 
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
+use pocketmine\plugin\PluginOwned;
+use pocketmine\plugin\Plugin;
 use pocketmine\player\Player;
 
 use Terpz710\EconomyPE\Money;
 
-class TopMoney extends Command
+class TopMoney extends Command implements PluginOwned
 {
-    public function __construct()
+
+    private $plugin;
+    
+    public function __construct(Money $plugin)
     {
+        $this->plugin = $plugin;
         $command = explode(":", Money::getConfigReplace("topmoney"));
         parent::__construct($command[0]);
         if (isset($command[1])) $this->setDescription($command[1]);
         $this->setAliases(Money::getConfigValue("topmoney_aliases"));
-        $this->setPermission("economype.topmoney");
+        $this->setPermission("economype.cmd.topmoney");
+    }
+
+    public function getOwningPlugin(): Plugin {
+        return $this->plugin;
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args)
