0ff4eaa9a6cc1e8ea58e3bd741e4d06204d46cbc
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-12-23 12:48:44 -0800

    AddMoney - PluginOwned and permission

--- src/Terpz710/EconomyPE/Commands/AddMoney.php
+++ src/Terpz710/EconomyPE/Commands/AddMoney.php
@@ -6,20 +6,27 @@ namespace Terpz710\EconomyPE\Commands;
 
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
+use pocketmine\plugin\PluginOwned;
+use pocketmine\plugin\Plugin;
 use pocketmine\player\Player;
 use pocketmine\Server;
 
 use Terpz710\EconomyPE\Money;
 
-class AddMoney extends Command
+class AddMoney extends Command implements PluginOwned
 {
-    public function __construct()
+    public function __construct(Money $plugin)
     {
+        $this->plugin = $plugin;
         $command = explode(":", Money::getConfigReplace("addmoney"));
         parent::__construct($command[0]);
         if (isset($command[1])) $this->setDescription($command[1]);
         $this->setAliases(Money::getConfigValue("addmoney_aliases"));
-        $this->setPermission("economype.addmoney");
+        $this->setPermission("economype.cmd.addmoney");
+    }
+
+    public function getOwningPlugin(): Plugin {
+        return $this->plugin;
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args)
